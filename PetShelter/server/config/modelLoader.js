cb47f6111ad4b00ff1fa6ed6f1bb312dd6debb74
Caleb Reyes <sciencecamp123@gmail.com>
2020-01-27 15:38:33 -0800

    Changed db connection

--- PetShelter/server/config/modelLoader.js
+++ PetShelter/server/config/modelLoader.js
@@ -12,7 +12,7 @@ let mydb = "sample"
 
 
 
-mongoose.connect(`mongodb+srv://cakedayy:root@calebcluster-boj0y.mongodb.net/PetShelter?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology:true});
+mongoose.connect('mongodb://localhost/PetShelter', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('useCreateIndex', true);
 
 var models_path = path.join(__dirname, './../models');
